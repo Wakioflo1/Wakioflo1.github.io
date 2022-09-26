98b41642ab372c9676a234f38c5cc7de749ca1c4
Wakioflo1 <wakioof18@gmail.com>
2022-09-26 10:39:16 +0300

    ADDED MY PROJECT

--- js/custom.js
+++ js/custom.js
@@ -1,55 +1,47 @@
 
-  $(function () {
-    'use strict'
+// GOOGLE MAP
+var map = '';
+var center;
 
-    // MENU
-    $('.navbar .nav-link').on('click',function(){
+function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(13.758468, 100.567481),
+      scrollwheel: false
+    };
+  
+    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
+    });
+  
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+}
+
+function calculateCenter() {
+  center = map.getCenter();
+}
+
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
+
+$(function(){
+  loadGoogleMap();
+});
+
+// NIVO LIGHTBOX
+$('#portfolio a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+   $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
-
-    $(window).on('scroll', function() {     
-                                
-        /*----------------------------------------------------*/
-        /*  Navigtion Menu Scroll
-        /*----------------------------------------------------*/    
-        
-        var b = $(window).scrollTop();
-        
-        if( b > 72 ){       
-            $(".navbar").addClass("scroll");
-        } else {
-            $(".navbar").removeClass("scroll");
-        }               
-    });
-
-    // TESTIMONIALS CAROUSEL
-    $('#testimonials-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            900:{
-                items:2,
-            },
-            1200:{
-                items:3,
-                loop:false
-            }
-        }
-    })
-
-    // SMOOTHSCROLL
-    $(function() {
-      $('.navbar .nav-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });   
-     
-  });
